35e3e3789a1a0d1fea2f458ff631832c5874660b
Wes Rowe <wesleyrowe@gmail.com>
2013-05-07 22:13:43 -0700

    scroll-fixed working, but...
    I'm going to mess around now with setting the height and then adding
    overflow: scroll.

--- common-js/interaction.js
+++ common-js/interaction.js
@@ -1,21 +1,21 @@
 $(document).ready( function() {
 	
 	/* fixed-scroll action for key */
-	/* var theLoc = $('#lower_left_bar').position().top;
-	var left_offset = $('#lower_left_bar').position().left;
-    $(window).scroll(function() {
+	var theLoc = $('#lower_left_bar').offset().top;
+	//var left_offset = $('#dynamic_car_display').position().left;
+    $(window).scroll( function() {
         if(theLoc >= $(window).scrollTop()) {
             if($('#lower_left_bar').hasClass('fixed')) {
                 $('#lower_left_bar').removeClass('fixed');
 				
             }
         } else { 
-            if(!$('#lower_left_bar').hasClass('fixed')) {
+            if( !$('#lower_left_bar').hasClass('fixed') ) {
                 $('#lower_left_bar').addClass('fixed');
 				
             }
         }
-    });  */
+    }); 
 	
 	// end fixed-scroll
 	
